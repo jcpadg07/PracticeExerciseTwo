0f98d2a1d3087f3a2c7f3dffe179d00fbf73c1fa
James Padgett <jampad14@gmail.com>
2019-01-23 19:46:43 -0500

    part one done

---
+++ PracticeExercise2/PracticeExercise2/IShark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercise2
+{
+    interface IShark
+    {
+        void Bite();
+        void Swim(int speed);
+    }
+}
---
+++ PracticeExercise2/PracticeExercise2/Shark.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercise2
+{
+    public class Shark : IShark
+    {
+        public void Bite() {
+
+        }
+
+        public void Swim(int speed) {
+
+        }
+    }
+}
